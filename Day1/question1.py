9d164fcc4020e9a7a1bb9c9b57f8159c62837c4e
=Raju9934 <raju.gupta10u@gmail.com>
2024-08-16 01:59:10 +0530

    updated code

--- Day1/question1.py
+++ Day1/question1.py
@@ -1,16 +1,13 @@
-def add_without_arithmetic_operators(a, b):
-    while b != 0:
-       
-        carry = a & b
+def add_without_arithmatic_operator(a,b):
+    while b!=0:
+        carry=a&b
+        a=a^b
+        b=carry<<1
         
-        
-        a = a ^ b
-        
-        
-        b = carry << 1
-    
+
     return a
 
+result=add_without_arithmatic_operator(12,18)
+print(result)
 
-result = add_without_arithmetic_operators(15, 25)
-print(result)  
+  
